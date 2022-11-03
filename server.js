6c988b3a5d6c2a5d4f454ac39bd574ba79fde088
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2022-11-03 13:05:27 -0300

    commit antes de la entrega

--- server.js
+++ server.js
@@ -1,59 +1,82 @@
 const express = require("express");
-
-
 const app = express();
+const { Router } = express;
+const Contenedor = require("./contenedor");
+const contenedor = new Contenedor();
+const routerProducts = Router();
+const bodyParser = require("body-parser");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
+app.use("/api/productos", routerProducts);
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.listen(port, () =>
+    console.log(`The server is running in: http://localhost:${port} 🔐`)
+);
+app.get("/", (req, res) => {
+    res.send(
+        "<h1 style='color:blue;'>Welcome to one of the biggest e-commerce around US</h1>"
+    );
+});
+app.get("/formulario", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html"); //respondemos con un archivo
+});
 
+app.post("/formulario", (req, res) => {
+    const { body } = req;
+    console.log(body);
+    contenedor.save(body);
+    res.send("Thank you for sharing your data!");
+});
 
+routerProducts.get("", async (req, res) => {
+    const products = await contenedor.getAll();
+    res.json(products);
+});
 
-
-class Contenedor {
-    constructor (fileName){
-        this.fileName = fileName;
+routerProducts.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const products = await contenedor.getAll();
+    const product = await contenedor.getById(id);
+    if (id > products.length) {
+        res.json({
+            error: "This product was not found",
+            productList: products,
+        });
+    } else {
+        res.json(product);
     }
-  
-    async getAll(){
-        const fs = require('fs');
-        const ruta = this.fileName;
-        try {
-            const archivo = await fs.promises.readFile(ruta, 'utf-8');
-            const productos = JSON.parse(archivo);
-            return productos;
-            
-        } catch (error) {
-            console.log('Se ha producido un error en getAll()', 'error numero: ', error);
-        } 
-               
+});
+
+routerProducts.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const products = await contenedor.getAll();
+
+    if (id > products.length) {
+        res.json({
+            error: "This product was not found",
+            productList: products,
+        });
+    } else {
+        await contenedor.deleteById(id);
+        res.json({
+            success: true,
+            msg: "This product was deleted successfully.",
+        });
     }
-  
-  }
+});
 
-const getRandomInt = (max) => {
-  return 
-}
-
-let numero = getRandomInt(5);
-
-
-
-app.get("/productos", async(req,res)=>{
-    const contenedorProductos = new Contenedor("productos.txt");
-    const allProducts = await contenedorProductos.getAll()
-    res.send(allProducts)
-})
-
-app.get("/productoRandom", async(req,res)=>{
-    const contenedorProductos = new Contenedor("productos.txt");
-    const allProducts = await contenedorProductos.getAll()
-    const numRandom = Math.floor(Math.random() * (allProducts.length));
-    const productoRandom = allProducts[numRandom]
-    res.send(productoRandom)
-})
-
-app.get("/",(req,res)=>{
-    res.send(`Bienvenido al server!`)
-})
-
-
-app.listen(PORT,()=>console.log(`server listeing on port ${PORT}`));
+routerProducts.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, price, thumbnail } = req.body;
+        await contenedor.updateById(id, title, price, thumbnail);
+        res.json({ succes: true });
+    } catch (error) {
+        res.json({ error: true, msj: "error" });
+    }
+});
